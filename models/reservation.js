02281f2ab0329eb2b6833c95c45ce116b1031714
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-08 17:16:22 -0800

    Created .save() method for reservations

--- models/reservation.js
+++ models/reservation.js
@@ -22,6 +22,26 @@ class Reservation {
     return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
   }
 
+  /** save this reservation. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3
+             WHERE id=$4`,
+        [this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
+
   /** given a customer id, find their reservations. */
 
   static async getReservationsForCustomer(customerId) {
